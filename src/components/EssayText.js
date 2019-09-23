9b2618bc1366cc52c202aa980d8134362c544bff
cutofmyjib <thedianalozano@gmail.com>
2019-09-22 21:46:04 -0500

    feat: replace string marker with user input, bold

--- src/components/EssayText.js
+++ src/components/EssayText.js
@@ -7,26 +7,28 @@ import { getTextTemplates } from "../helpers";
 require("./EssayText.scss");
 
 function Text(data) {
-    let sentence;
+    let string;
     switch (data.fieldname) {
         case FIELD_NAMES.hometown: 
-            sentence = `${getTextTemplates(FIELD_NAMES.hometown)[6]}`;
+            string = `${getTextTemplates(FIELD_NAMES.hometown)[6]}`;
             break;
         case FIELD_NAMES.favoriteFood: 
-            sentence = `${getTextTemplates(FIELD_NAMES.favoriteFood)[0]}`;
+            string = `${getTextTemplates(FIELD_NAMES.favoriteFood)[0]}`;
             break;
         case FIELD_NAMES.loveToDo: 
-            sentence = `${getTextTemplates(FIELD_NAMES.loveToDo)[3]}`;
+            string = `${getTextTemplates(FIELD_NAMES.loveToDo)[3]}`;
             break;
         case FIELD_NAMES.messageIf: 
-            sentence = `${getTextTemplates(FIELD_NAMES.messageIf)[3]}`;
+            string = `${getTextTemplates(FIELD_NAMES.messageIf)[3]}`;
             break;
             
         
     }
 
+    const [before, after] = string.split("$answer");
+
     return (
-        data.answer ? <span>{sentence} <b>{data.answer}</b>. </span> : null
+        data.answer ? <span>{before} <strong>{data.answer}</strong>{after} </span> : null
     );
 }
 
@@ -36,11 +38,13 @@ function EssayText(data) {
         <div className="essay-text">
             <div>
                 <h1>Your essay text</h1>
-                <Text fieldname='hometown' answer={data.fieldAnswers.hometown}/>
-                <Text fieldname='favoriteFood' answer={data.fieldAnswers.favoriteFood}/>
-                <Text fieldname='loveToDo' answer={data.fieldAnswers.loveToDo}/>
-                <Text fieldname='messageIf' answer={data.fieldAnswers.messageIf}/>
-                <div>{showButton ? <Button text="Edit"/> : null}</div>
+                <div className="content">
+                    <Text fieldname='hometown' answer={data.fieldAnswers.hometown}/>
+                    <Text fieldname='favoriteFood' answer={data.fieldAnswers.favoriteFood}/>
+                    <Text fieldname='loveToDo' answer={data.fieldAnswers.loveToDo}/>
+                    <Text fieldname='messageIf' answer={data.fieldAnswers.messageIf}/>
+                </div>
+                {showButton ? <Button text="Edit"/> : null}
             </div>
         </div>
     )
